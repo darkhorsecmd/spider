7411d60cdea9ae6ff18351c85799f2481bc17843
darkhorsecmd <1368306623@qq.com>
2019-02-20 22:38:28 +0800

    明天更新下 存入mysql数据库

--- spider/DbSave/save.py
+++ spider/DbSave/save.py
@@ -1,3 +1,7 @@
 import os
+
 if __name__ == '__main__':
-    path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  #E:\19年文件\毕设爬虫\spider\spider
+    print(os.path.dirname(__file__))
+    path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  # E:\19年文件\毕设爬虫\spider\spider
+    detail_path = path + "\\xmlUnit+\\linkList_detail"
+    print(detail_path)
